007690b18fb3da85dee14ba8e985b2f342a1ce02
Ryan Kass <r954k200@ku.edu>
2017-09-24 14:22:45 -0500

    Completed Exercise1

---
+++ Password.js
@@ -0,0 +1,18 @@
+function password(password1,password2)
+{
+  if(password1 == null || password1 == "" || password1.length < 8)
+  {
+    alert("Password needs to be at least 8 characters long.");
+  }
+  else
+  {
+    if(password1 == password2)
+    {
+      alert("Your password is " + password1);
+    }
+    else
+    {
+      alert("The entered passwords don't match.");
+    }
+  }
+}
